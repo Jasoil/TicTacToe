179ea6aa7bacc432dc6875dca13ba36a6e588f64
Jaani Söderström <jisode@utu.fi>
2022-12-08 17:44:19 +0200

    symbolin ja paikan valinta + tietokone valinta

--- Ristinolla/main.py
+++ Ristinolla/main.py
@@ -1,20 +1,53 @@
-from funktiot import printlauta
-
-#interface
-
-#print("Tervetuloa")
-#while True:
- #       token = input("Valitse pelimerkki syöttämällä x tai 0 (nolla): ")
-  #      if token != "x" or token != 0:
-   #         print("Syötteen on oltava x tai 0!")
-    #        continue
-     #   else:
-      #      break
-#uinput = input("Syötä numero, jonka paikalle haluat sijoittaa pelimerkin: ")
-#board
-
-#change board
-#board[0][0] = token
+from metodit import *
+import random
+import time
 
 
+print("\n***Tervetuloa***\n")
+time.sleep(1)
+while True:
+        psymboli = input("Valitse pelimerkki syöttämällä x tai 0 (nolla): ")
+        if (psymboli == "x") or (psymboli == "0"):
+            time.sleep(0.5)
+            print("\n***Lukitaan symboli: " + psymboli + "***")
+            time.sleep(2)
+            if psymboli == "x":
+                  bsymboli = "0"
+            elif psymboli == "0":
+                  bsymboli = "x"
+            break
+        else:
+            print("Syötteen on oltava x tai 0!")
+            continue
+      #botti
 printlauta()
+
+#pelaaja
+vuoro = 0
+while(vuoro != "null"):
+      while(vuoro == 0):
+            psiirto = input("\nSyötä numero, jonka paikalle haluat sijoittaa pelimerkin tai 'lopeta' lopettaaksesi: ")
+            if psiirto == "lopeta":
+                  vuoro = "null"
+                  lopeta()
+            elif int(psiirto) < 1 or int(psiirto) > 9:
+                  print("Siirron tulee olla 0-9 väliltä!") 
+                  break
+            elif psiirto != "lopeta":
+                  print("Valitaan ruutu " + psiirto)
+                  time.sleep(1)
+                  vuoro = 1
+                  muutalauta(int(psiirto), psymboli)
+                  printlauta()
+                  time.sleep(2)
+                  print("\n***Tietokoneen vuoro***")
+                  time.sleep(1)
+                  while(vuoro == 1):
+                        print("Tietokone valitsee...")
+                        time.sleep(2)
+                        bsiirto = random.randint(1,9)
+                        muutalauta(bsiirto, bsymboli)
+                        printlauta()
+                        vuoro = 0
+                        print("\n***Pelaajan vuoro***")
+
--- Ristinolla/metodit.py
+++ Ristinolla/metodit.py
@@ -1,6 +1,6 @@
-#laudan signatuuri
+#laudan attribuutit
 lauta = [[1,2,3], [4,5,6], [7,8,9]]
-numerot = [1,2,3,4,5,6,7,8,9]
+paikat = [1,2,3,4,5,6,7,8,9]
 rivit = 3
 sarakkeet = 3
 
@@ -16,22 +16,25 @@ def muutalauta(valinta, symboli):
     #rivi 1
     if(valinta == 1):
         lauta[0][0] = symboli
-    if(valinta == 2):
+    elif(valinta == 2):
         lauta[0][1] = symboli
-    if(valinta == 3):
+    elif(valinta == 3):
         lauta[0][2] = symboli
     #rivi 2
-    if(valinta == 4):
+    elif(valinta == 4):
         lauta[1][0] = symboli
-    if(valinta == 5):
+    elif(valinta == 5):
         lauta[1][1] = symboli
-    if(valinta == 6):
+    elif(valinta == 6):
         lauta[1][2] = symboli
     #rivi3
-    if(valinta == 6):
+    elif(valinta == 7):
         lauta[2][0] = symboli
-    if(valinta == 7):
+    elif(valinta == 8):
         lauta[2][1] = symboli
-    if(valinta == 8):
+    elif(valinta == 9):
         lauta[2][2] = symboli
-
+    
+def lopeta():
+      print("***Peli päättyi***")
+      exit
